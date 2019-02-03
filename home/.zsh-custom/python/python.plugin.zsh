e6384d713cfa27cefae2aba0266bab6d8136f22d
parnurzeal <parnurzeal@gmail.com>
2019-02-03 16:50:54 +0800

    Add pyenv-virtualenv setting

--- home/.zsh-custom/python/python.plugin.zsh
+++ home/.zsh-custom/python/python.plugin.zsh
@@ -2,3 +2,9 @@
 if [[ -n "$OS_OSX" ]]; then
   PATH="$PATH:/Users/twattanavekin/Library/Python/2.7/bin"
 fi
+
+# Add virtualenv (refer: https://github.com/pyenv/pyenv-virtualenv)
+# TODO: Check if not install pyenv and pyenv-virtualenv, automatically install it.
+echo "[pyenv] Initialize virtualenv for pyenv"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
