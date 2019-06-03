2837c55c34cf7287013e4346711be916c5d6241b
parnurzeal <parnurzeal@gmail.com>
2019-06-03 14:21:11 +0800

    Exported flutter-web in $PATH

--- home/.zsh-custom/flutter/flutter.plugin.zsh
+++ home/.zsh-custom/flutter/flutter.plugin.zsh
@@ -1,7 +1,8 @@
 # Set flutter bin path
 if [[ "${DOT_OS}" == "OSX" ]]; then
   echo "[flutter] Set flutter bin path in OSX."
-  export PATH=$PATH:~/Applications/flutter/bin
+  export PATH="$PATH":"$HOME/Applications/flutter/bin"
+  export PATH="$PATH":"$HOME/Applications/flutter/.pub-cache/bin"
 fi
 
 # TODO: Check if not install flutter, then automatically install it.
