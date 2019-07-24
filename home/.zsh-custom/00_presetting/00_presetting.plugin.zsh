020f0ae691f491eb0e9f0ac0ae7fe13e2296f811
parnurzeal <parnurzeal@gmail.com>
2019-07-24 15:51:02 +0800

    Add function to add available cmd tools to my global note

--- home/.zsh-custom/00_presetting/00_presetting.plugin.zsh
+++ home/.zsh-custom/00_presetting/00_presetting.plugin.zsh
@@ -1,1 +1,8 @@
 # This script runs first before any other plugin.
+
+# For adding available cmd tools to A's list.
+check_cmd_exists_and_add_note() {
+  if type "$1" > /dev/null ; then
+    export A_COMMANDS="${A_COMMANDS}\n$2"
+  fi
+}
